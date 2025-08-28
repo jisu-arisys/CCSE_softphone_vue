730a236e9ba269e7ad67fcf827c681bf4bf07350
jisu_arisys <jisu_um@arisys.co.kr>
2025-08-28 18:11:15 +0900

    base

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import './css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './css/style.css'
 createApp(App).mount('#app')
