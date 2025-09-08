851ce4c49817f94cf3953a8cd188483f579ed315
jisu_arisys <jisu_um@arisys.co.kr>
2025-09-08 13:20:48 +0900

    feat : 프로젝트 실행 가이드 문서 작성

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,8 @@ import fs from 'fs'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    // https://zoom.us/cci/index/admin#/admin-integrations 에서 설정한 스마트 내장 인스턴스 URL 경로
-    host: '192.168.10.75', // 외부접속 허용
+    host: '192.168.10.75',
     port: 4033,
-    // HTTPS 설정 필수
-    // https: true
     https: {
       key: fs.readFileSync('./certs/key.pem'),
       cert: fs.readFileSync('./certs/cert.pem')
