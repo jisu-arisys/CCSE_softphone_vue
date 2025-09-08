75f2ad85c92f357677fcb5ab235c1e99a391213a
jisu_arisys <jisu_um@arisys.co.kr>
2025-09-08 13:05:32 +0900

    chore : 통화상태 확인요청 버튼 위치수정

--- src/App.vue
+++ src/App.vue
@@ -49,20 +49,19 @@
 
           <div class="card m-3 p-3 shadow-sm">
             <div class="row">
-              <div class="col-5">
+              <div class="col-3">
                 <div class="card-title"> 상담원 상태출력 </div>
                 <div class="card-body d-flex justify-content-between align-items-center">
                   <span id="agent-status-label" class="badge rounded-pill text-lg" :class="statusDropdownClass">{{ agentStatusDisplay }}</span>
                   <template v-if="activeCallId !=null && agentStatusDisplay === 'Occupied'">
                     <span class="badge rounded-pill"  :class="getStateBadgeClass(zccEngagementCache[activeCallId].state)">{{ zccEngagementCache[activeCallId].state }}</span>
                   </template>
-                  <button class="btn btn-toolbar btn-sm text-danger" style="font-size: 10px" type="button" @click.prevent="requestEngagementStatus()">통화상태 확인</button>
                 </div>
                 <div class="card-subtitle text-secondary p-2" style="font-size: 10px">
                   <span class="text-primary">Occupied</span> 상태에서 상담완료시, <span class="text-success">Ready</span>로 전환됩니다. 이석 상태에서 상담완료시에는 상태가 유지됩니다.
                 </div>
               </div>
-              <div class="col-7">
+              <div class="col-6">
                 <div class="card-title"> 상담원 상태변경</div>
                 <div class="card-body">
                   <button class="btn btn-light m-1 text-success" @click="setAgentStatus('1')">
@@ -74,10 +73,15 @@
                   </button>
                 </div>
               </div>
+              <div class="col-3">
+                <div class="card-title"> 상담원 상태확인</div>
+                <div class="card-body">
+                  <button class="btn btn-outline-secondary m-1" type="button" @click.prevent="requestEngagementStatus()">통화 확인</button>
+                  <button class="btn btn-outline-secondary m-1" type="button" @click.prevent="getAgentStatus()">상태 확인</button>
+                </div>
+              </div>
             </div>
           </div>
-
-
           <div class="row m-1">
             <div class="col-6">
               <div class="card p-3 shadow-sm">
